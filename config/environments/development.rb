acc2f4b8ce519c7e66f9c60f61b0aac2b731c6c4
Trent <trentshields1989@gmail.com>
2016-09-16 10:38:55 +1000

    devise implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+
+  # Devise code that sends mail to the terminal. Implemented on devise install.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
